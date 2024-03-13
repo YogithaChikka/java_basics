cc622ea5edceb3f6b981887fc85bfcf941c3b4ef
yogitha <yogithachikka1821@gmail.com>
2024-03-13 21:55:58 +0530

    CHANGE

--- Main.java
+++ Main.java
@@ -1,5 +1,30 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+public class ScalerAssignments
+{
+    public static void main(String[] args)
+    {
+
+       Scanner sc = new Scanner(System.in);
+       int N = sc.nextInt();
+
+//        /*   WHILE LOOP  */
+
+        int oddIndexSum = 0;
+        int evenIndexSum = 0;
+//      calculating the index digit from right to left
+        int index = 1;
+        while (N>0) {
+            int ld = N % 10;
+            N/=10;
+            if (index % 2 == 0) {
+                evenIndexSum += ld;
+            }
+            else {
+                oddIndexSum += ld;
+            }
+            index++;
+        }
+        System.out.println("Sum of Odd Index Digit : " + oddIndexSum);
+        System.out.println("Sum of Even Index Digit : " + evenIndexSum);
     }
 }
