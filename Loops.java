32825ac0bb4beedb0cc5c4b782b650e76e9881ac
yogitha <yogithachikka1821@gmail.com>
2024-03-21 12:22:04 +0530

    Change

--- Loops.java
+++ Loops.java
@@ -68,7 +68,6 @@
 
 // //        /*  FOR LOOP  */
 
-// //      (My method)
 //         for (int i = 1; i <= N ; i++) {
 //             System.out.print(i + " ");
 //         }
@@ -91,7 +90,6 @@
 
 // //        /*  FOR LOOP  */
 
-// //      (My method)
 //         for (int i = N; i >= 1 ; i--) {
 //             System.out.print(i + " ");
 //         }
@@ -111,11 +109,92 @@
 
 // //        /*  FOR LOOP  */
 
-// //      (My method)
 //         int sum = 0;
 //         for (int i = 1; i <= N ; i++) {
 //             sum += i;
 //         }
 //         System.out.print(sum);
+//     }
+// }
+
+
+                                                          
+// PALINDROMIC INTEGER (21-03-2024) [for loop-1 Additional problems 1]                             
+                             
+                             
+// import java.util.Scanner;
+// public class ScalerAssignments
+// {
+//     public static void main(String[] args)
+//     {
+
+//        Scanner sc = new Scanner(System.in);
+//        int A = sc.nextInt();
+
+// //        /*  FOR LOOP  */
+
+//         int num = A;
+//         int rev = 0;
+//         int ld = 0;
+//         for (; A > 0; A/=10) {
+//              ld = A%10;
+//             rev = rev * 10 + ld;
+//         }
+
+//         if (num==rev) {
+//             System.out.println("Yes");
+//         }
+//         else {
+//             System.out.println("No");
+//         }
+//     }
+// }    
+
+
+
+// ODD GAME (21-03-2024) [for loop-1 Additional problems 2]      
+      
+            
+// import java.util.Scanner;
+// public class ScalerAssignments
+// {
+//     public static void main(String[] args)
+//     {
+
+//        Scanner sc = new Scanner(System.in);
+//        int N = sc.nextInt();
+
+// //        /*  FOR LOOP  */
+
+//         for (int i = 1; i <= N; i++) {
+//             if (i%2!=0) {
+//                 System.out.print(i + " ");
+//             }
+//         }
+
+//     }
+// }         
+
+
+
+// EVEN GAME (21-03-2024) [for loop-1 Additional problems 3]                              
+                              
+                              
+// import java.util.Scanner;
+// public class ScalerAssignments
+// {
+//     public static void main(String[] args)
+//     {
+
+//        Scanner sc = new Scanner(System.in);
+//        int N = sc.nextInt();
+
+// //        /*  FOR LOOP  */
+
+//         for (int i = 1; i <= N; i++) {
+//             if (i%2==0) {
+//                 System.out.print(i + " ");
+//             }
+//         }
 //     }
 // }
