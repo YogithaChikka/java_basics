6d03e922c4e2cf1dd05880594c11402ede8086c3
yogitha <yogithachikka1821@gmail.com>
2024-03-23 19:08:43 +0530

    Change

--- Loops.java
+++ Loops.java
@@ -198,3 +198,107 @@
 //         }
 //     }
 // }
+
+
+
+// EASY POWER (22-03-2024) [for loop-1 Additional problems 4]      
+  
+      
+// import java.util.Scanner;
+// public class ScalerAssignments
+// {
+//     public static void main(String[] args)
+//     {
+
+//        Scanner sc = new Scanner(System.in);
+//        int A = sc.nextInt();
+//        int B = sc.nextInt();
+
+// //        /*  FOR LOOP  */
+
+//         int ans = 1;
+//         for (int i = 1; i <= B; i++) {
+//             ans *= A;
+//         }
+//         System.out.println(ans);
+//     }
+// }
+
+
+
+// MULTIPLICATION TABLE (22-03-2024) [for loop-1 Additional problems 5]                              
+
+                              
+// import java.util.Scanner;
+// public class ScalerAssignments
+// {
+//     public static void main(String[] args)
+//     {
+
+//        Scanner sc = new Scanner(System.in);
+//        int A = sc.nextInt();
+
+// //        /*  FOR LOOP  */
+
+//         for (int i = 1; i <= 10; i++) {
+//             System.out.println(A + " * " + i + " = " + (A*i));
+//         }
+//     }
+// } 
+
+
+
+// COUNT FACTORS (23-03-2024) [for loop-2 Assignment problems 1]      
+
+      
+// import java.util.Scanner;
+// public class ScalerAssignments
+// {
+//     public static void main(String[] args)
+//     {
+
+//        Scanner sc = new Scanner(System.in);
+//        int N = sc.nextInt();
+
+// //        /*  FOR LOOP  */
+
+//         int count = 0;
+//         for (int i = 1; i <= N; i++) {
+//             if (N%i==0) {
+//                 count++;
+//             }
+//         }
+//         System.out.println(count);
+//     }
+// } 
+                              
+                              
+                              
+// IS IT PRIME? (23-03-2024) [for loop-2 Assignmennt problems 2]
+                              
+                              
+// import java.util.Scanner;
+// public class ScalerAssignments
+// {
+//     public static void main(String[] args)
+//     {
+
+//        Scanner sc = new Scanner(System.in);
+//        int A = sc.nextInt();
+
+// //        /*  FOR LOOP  */
+
+//         int countPrime = 0;
+//         for (int i = 1; i <= A; i++) {
+//             if (A % i == 0) {
+//                 countPrime++;
+//             }
+//         }
+//         if (countPrime==2) {
+//             System.out.println("YES");
+//         }
+//         else {
+//             System.out.println("NO");
+//         }
+//     }
+// }                              
---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 10; i <= 5; i--) {
+             System.out.print(i + " ");
+         }
+     }
+}
